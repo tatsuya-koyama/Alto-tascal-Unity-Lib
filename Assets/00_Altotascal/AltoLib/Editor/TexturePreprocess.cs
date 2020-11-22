6f3cb6c77082a68e6a58b744a282cd257af945b6
Tatsuya Koyama <tatsuya.koyama.public@gmail.com>
2020-11-22 22:50:34 +0900

    Add TextureReimporter

---
+++ Assets/00_Altotascal/AltoLib/Editor/TexturePreprocess.cs
@@ -0,0 +1,38 @@
+﻿#if ALTO_DEV
+// Define "ALTO_DEV" symbol to enable this preprocess script.
+using UnityEditor;
+using UnityEngine;
+
+namespace AltoLib
+{
+    /// <summary>
+    /// プロジェクトにテクスチャアセットを追加した際、
+    /// iOS / Android 向け圧縮フォーマットを自動で設定するサンプルコード。
+    /// 各テクスチャの初回 Import 時に実行される。
+    ///
+    /// フォーマットは TextureReimporter で指定したものが適用される。
+    /// </summary>
+    public class TexturePreprocess : AssetPostprocessor
+    {
+        void OnPreprocessTexture()
+        {
+            // 初回 Import 時のみ処理を行う。
+            // 以前は .meta ファイルの存在確認で判定できていたが、
+            // Unity 2019.4 では Preprocess の段階で .meta ファイルが作られていた。
+            // 初回は importSettingsMissing が True になるようなのでそちらで判定する。
+            var textureImporter = (TextureImporter)assetImporter;
+            if (!textureImporter.importSettingsMissing) { return; }
+
+            Debug.Log($"New texture is detected : {assetPath}");
+            if (textureImporter == null)
+            {
+                Debug.LogError($"TextureImporter not found : {assetPath}");
+                return;
+            }
+
+            TextureReimporter.SetImportSettingsForIos(textureImporter, assetPath);
+            TextureReimporter.SetImportSettingsForAndroid(textureImporter, assetPath);
+        }
+    }
+}
+#endif
---
+++ Assets/00_Altotascal/AltoLib/Editor/TextureReimporter.cs
@@ -0,0 +1,123 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AltoLib
+{
+    /// <summary>
+    /// テクスチャフォーマットを一括変換するエディタ拡張。
+    /// フォーマット形式などの設定は外出ししていないので、カスタムしたい場合は
+    /// スクリプトをコピーして適宜書き換えてほしい。
+    /// </summary>
+    public class TextureReimporter
+    {
+        //----------------------------------------------------------------------
+        // Convert Settings
+        //----------------------------------------------------------------------
+
+        const TextureImporterFormat IosTextureFormat     = TextureImporterFormat.ASTC_6x6;
+        const TextureImporterFormat AndroidTextureFormat = TextureImporterFormat.ETC2_RGBA8;
+        static readonly string[] TargetFolders = {"Assets"};
+
+        public static void SetImportSettingsForIos(TextureImporter textureImporter, string assetPath)
+        {
+            int originalMaxSize = textureImporter.maxTextureSize;
+            textureImporter.SetPlatformTextureSettings(new TextureImporterPlatformSettings
+            {
+                name               = "iPhone",
+                overridden         = true,
+                maxTextureSize     = originalMaxSize,
+                resizeAlgorithm    = TextureResizeAlgorithm.Mitchell,
+                format             = IosTextureFormat,
+                textureCompression = TextureImporterCompression.Compressed,
+                compressionQuality = 50,
+            });
+            Debug.Log($"{assetPath} [iOS] : Set {IosTextureFormat.ToString()}");
+        }
+
+        public static void SetImportSettingsForAndroid(TextureImporter textureImporter, string assetPath)
+        {
+            int originalMaxSize = textureImporter.maxTextureSize;
+            textureImporter.SetPlatformTextureSettings(new TextureImporterPlatformSettings
+            {
+                name               = "Android",
+                overridden         = true,
+                maxTextureSize     = originalMaxSize,
+                resizeAlgorithm    = TextureResizeAlgorithm.Mitchell,
+                format             = AndroidTextureFormat,
+                textureCompression = TextureImporterCompression.Compressed,
+                compressionQuality = 50,
+            });
+            Debug.Log($"{assetPath} [Android] : Set {AndroidTextureFormat.ToString()}");
+        }
+
+        //----------------------------------------------------------------------
+        // private
+        //----------------------------------------------------------------------
+
+        const string ProgressBarTitle = "Update Texture Format";
+
+        /// <summary>
+        /// ゲームに使用されているテクスチャの iOS / Android 向け圧縮フォーマットを統一する。
+        /// テクスチャの Max Size は既存の設定をそのまま引き継ぎ、圧縮フォーマットのみを指定、
+        /// 変更があったものについてアセットをインポートし直す。
+        /// </summary>
+        [MenuItem("Alto/Convert Texture Format to ASTC or ETC2")]
+        static void UpdateTextureCompressionSetting()
+        {
+            string[] guids = AssetDatabase.FindAssets("t:texture2D", TargetFolders);
+
+            try
+            {
+                EditorUtility.DisplayProgressBar(ProgressBarTitle, "", 0f);
+                for (var i = 0; i < guids.Length; ++i)
+                {
+                    string guid = guids[i];
+                    string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                    SetTextureCompression(assetPath, i, guids.Length);
+                }
+
+                EditorUtility.DisplayProgressBar(ProgressBarTitle, "Refresh AssetDatabase ...", 1f);
+                AssetDatabase.Refresh();
+                Debug.Log("Texture format conversion is completed.");
+            }
+            finally
+            {
+                EditorUtility.ClearProgressBar();
+            }
+        }
+
+        static void SetTextureCompression(string assetPath, int count, int totalCount)
+        {
+            // Font テクスチャは除外
+            if (assetPath.EndsWith(".ttf")) { return; }
+            if (assetPath.EndsWith(".otf")) { return; }
+
+            var textureImporter = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+            if (textureImporter == null)
+            {
+                Debug.LogError($"TextureImporter not found : {assetPath}");
+                return;
+            }
+
+            if (!ShouldReimport(textureImporter)) { return; }
+            // Note. プログレスバーの表示はわずかに硬直時間があるようなので実際に処理する場合だけ表示
+            float progress = (float)count / totalCount;
+            EditorUtility.DisplayProgressBar(ProgressBarTitle, $"{assetPath} ({count} / {totalCount})", progress);
+
+            SetImportSettingsForIos(textureImporter, assetPath);
+            SetImportSettingsForAndroid(textureImporter, assetPath);
+            AssetDatabase.ImportAsset(assetPath);
+        }
+
+        static bool ShouldReimport(TextureImporter textureImporter)
+        {
+            var iosSettings = textureImporter.GetPlatformTextureSettings("iPhone");
+            if (iosSettings.format != IosTextureFormat || iosSettings.overridden == false) { return true; }
+
+            var androidSettings = textureImporter.GetPlatformTextureSettings("Android");
+            if (androidSettings.format != AndroidTextureFormat || androidSettings.overridden == false) { return true; }
+
+            return false;
+        }
+    }
+}
