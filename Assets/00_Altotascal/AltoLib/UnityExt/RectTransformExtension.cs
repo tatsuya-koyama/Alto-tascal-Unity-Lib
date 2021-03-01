7dd5f6352e3f9d410e987564edb67c5e33a3c063
Tatsuya Koyama <tatsuya.koyama.public@gmail.com>
2021-03-02 01:52:50 +0900

    Add RectTransform Extension

---
+++ Assets/00_Altotascal/AltoLib/UnityExt/RectTransformExtension.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace AltoLib.UnityExt
+{
+    public static class RectTransformExtension
+    {
+        static Vector2 _vec2;
+
+        //---------------------------------------------------------------------
+        // AnchoredPosition
+        //---------------------------------------------------------------------
+
+        public static void SetAnchoredPosition(this RectTransform rect, float? x, float? y)
+        {
+            _vec2.Set(
+                x ?? rect.anchoredPosition.x,
+                y ?? rect.anchoredPosition.y
+            );
+            rect.anchoredPosition = _vec2;
+        }
+
+        public static void AddAnchoredPosition(this RectTransform rect, float x, float y)
+        {
+            _vec2.Set(
+                rect.anchoredPosition.x + x,
+                rect.anchoredPosition.y + y
+            );
+            rect.anchoredPosition = _vec2;
+        }
+
+        public static void AddAnchoredPosition(this RectTransform rect, Vector2 vec)
+        {
+            rect.AddAnchoredPosition(vec.x, vec.y);
+        }
+    }
+}
